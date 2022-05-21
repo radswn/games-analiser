f7e5cea0ec19bcb97580243ec7f84c4ac860f69b
Oskar Szudzik <oszudzik@gmail.com>
2022-05-21 15:07:23 +0200

    Add pop-up with help, small table changes

--- ui.R
+++ ui.R
@@ -1,24 +1,28 @@
 library(shiny)
 library(ggplot2)
+library(shinyalert)
 
 
 ui <- dashboardPage(
   dashboardHeader(title = "Games analiser"),
   dashboardSidebar(disable = TRUE),
-  dashboardBody(fluidRow(
-    box(plotOutput("plot", height = 500),
-        title = "Average number of players"),
-    box(DT::dataTableOutput('table', height = 500))
-  ),
-  fluidRow(
-    box(shinydashboard::valueBoxOutput('ratings', width = 6)),
-    box(gaugeOutput('playersMeter')),
-  ),
-  fluidRow(
-    box(
-      shinydashboard::valueBoxOutput('achievements', width = 6),
-      shinydashboard::valueBoxOutput('playtime', width = 6)
-      
+  dashboardBody(
+    fluidRow(
+      useShinyalert(force = TRUE),
+      box(plotOutput("plot", height = 500),
+          title = "Average number of players"),
+      box(DT::dataTableOutput('table', height = 500))
+    ),
+    fluidRow(box(
+      shinydashboard::valueBoxOutput('ratings', width = 6)
+    ),
+    box(gaugeOutput('playersMeter')), ),
+    fluidRow(
+      box(
+        shinydashboard::valueBoxOutput('achievements', width = 6),
+        shinydashboard::valueBoxOutput('playtime', width = 6)
+        
+      )
     )
-  ))
+  )
 )
