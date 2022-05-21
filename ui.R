d86e056de2f884af003f1c025c54e01497d7fc59
Oskar Szudzik <oszudzik@gmail.com>
2022-05-21 13:09:37 +0200

    Add ratings box

--- ui.R
+++ ui.R
@@ -10,11 +10,15 @@ ui <- dashboardPage(
         title = "Average number of players"),
     box(DT::dataTableOutput('table', height = 500))
   ),
-  fluidRow(gaugeOutput('playersMeter')),
+  fluidRow(
+    box(shinydashboard::valueBoxOutput('ratings', width = 6)),
+    box(gaugeOutput('playersMeter')),
+  ),
   fluidRow(
     box(
       shinydashboard::valueBoxOutput('achievements', width = 6),
       shinydashboard::valueBoxOutput('playtime', width = 6)
+      
     )
   ))
 )
